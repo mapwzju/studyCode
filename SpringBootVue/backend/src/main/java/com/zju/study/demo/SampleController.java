10ee5f96418f95a45150c37067ab1acb46347273
PeiwenMa <mapwzju@gmail.com>
2017-12-30 21:37:45 +0800

    vue+spring boot环境搭建

---
+++ SpringBootVue/backend/src/main/java/com/zju/study/demo/SampleController.java
@@ -0,0 +1,26 @@
+package com.zju.study.demo;
+
+/**
+ * @author Arthur
+ * @Date 2017/12/30
+ * @Decription:
+ */
+import org.springframework.boot.*;
+import org.springframework.boot.autoconfigure.*;
+import org.springframework.stereotype.*;
+import org.springframework.web.bind.annotation.*;
+
+@Controller
+@EnableAutoConfiguration
+public class SampleController {
+
+    @RequestMapping("/hello")
+    @ResponseBody
+    String home() {
+        return "Hello World!";
+    }
+
+    public static void main(String[] args) throws Exception {
+        SpringApplication.run(SampleController.class, args);
+    }
+}
